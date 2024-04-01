985921f3b676d83ce3d9b50aca44bbd7ccd808ac
Zavier Christians <christz2@mail.lcc.edu>
2024-03-31 20:39:08 -0400

    My Chapter 9 Coding set

---
+++ coursesmod.py
@@ -0,0 +1,4 @@
+#define the directions function
+def directions():
+    #print the directions for the program
+    print('Enter your course below and the program will tell you tell you details about it.')
