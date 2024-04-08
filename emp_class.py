46e786132ccdb021e74b5b9a760312306888bdc8
Zavier Christians <christz2@mail.lcc.edu>
2024-04-07 21:15:28 -0400

    Added Comments

--- emp_class.py
+++ emp_class.py
@@ -1,32 +1,34 @@
+#define the employee class
 class employee:
+    #define the __init__ function with the name, ID_number, department, and job title as arguments
     def __init__(self, name, ID_number, department, job_title):
+        #set the name
         self.__name = name
+        #set the ID number
         self.__ID_number = ID_number
+        #set the department
         self.__department = department
+        #set the job title
         self.__job_title = job_title
-        
-    def set_name(self, name):
-        self.__name = input("Enter employee's name, ")
-        
-    def set_ID_number(self, ID_number):
-        self.__ID_number = input("Enter employee's ID number: ")
-        
-    def set_department(self, department):
-        self.__department = input("Enter employee's department, ")
-        
-    def set_job_title(self, job_title):
-        self.__job_title = input("Enter employee's job title, ")
-        
+    
+    #define the get_name method
     def get_name(self):
+        #return the name that was set by the programm
         return self.__name
     
+    #define the get_ID_number method
     def get_ID_number(self):
+        #return the ID number that was set by the programm
         return self.__ID_number
     
+    #define the get_department method
     def get_department(self):
+        #return the department that was set by the programm
         return self.__department
     
+    #define the get_job_title method
     def get_job_title(self):
+        #return the job title that was set by the programm
         return self.__job_title
                                  
         
--- pets.py
+++ pets.py
@@ -1,23 +1,40 @@
+#Class for setting a pets name, age, and type
 class pet: 
+    #defining the init method with the name, animal type, and age as the arguments
     def __init__(self, __name, __animal_type, __age):
+        #name attribute
         self.__name = __name
+        #animal type attribute
         self.__animal_type = __animal_type
+        #age attribute
         self.__age = __age
     
+    #defining the set name method
     def set_name(self):
+        #set the pets name to the users input
         self.__name = input("Write your pets name, ")
     
+    #defining the set animal type method
     def set_animal_type(self):
+        #Set the pets animal type to the users input
         self.__animal_type = input("Write what type of animal your is, ")
     
+    #defining the set age method
     def set_age(self):
+        #Set the pets age to the users input
         self.__age = input("Write your pets age, ")
     
+    #defining the get name method
     def get_name(self):
+        #print the pets name that the user input
         print(f'Pets name is: {self.__name}')
     
+    #defining the get animal type method
     def get_animal_type(self):
+        #print the pets animal type that the user input
         print(f'The pet is a: {self.__animal_type}')
     
+    #defining the get age method
     def get_age(self):
+        #print the pets age that the user input
         print(f'Pets age is: {self.__age}')
